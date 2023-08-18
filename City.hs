07cae390a8fd2bc599ed2f19c9fea16a9c076ffd
Lucaspini01 <pinil@udesa.edu.ar>
2023-08-18 09:12:41 -0300

    city

--- City.hs
+++ City.hs
@@ -1,8 +1,15 @@
 module City ( City, newC, nameC, distanceC )
    where
 
+import Point
+
 data City = Cit String Point deriving (Eq, Show)
 
 newC :: String -> Point -> City
+newC name point = Cit name point
+
 nameC :: City -> String
-distanceC :: City -> City -> Float
+nameC (Cit name point) = name
+
+distanceC :: City -> City -> Float -- la distancia entre dos ciudades usando la funcion difP
+distanceC (Cit _ point1) (Cit _ point2) = difP point1 point2
