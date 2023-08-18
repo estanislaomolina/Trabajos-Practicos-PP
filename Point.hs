fa6f62f2e49bfa16a5e6af5b7fb0ad784bdddebb
Estanislao Molina <estanislaomolina05@gmail.com>
2023-08-18 09:07:12 -0300

    Modulo Point

--- Point.hs
+++ Point.hs
@@ -5,3 +5,7 @@ data Point = Poi Int Int deriving (Eq, Show)
 
 newP :: Int -> Int -> Point
 difP :: Point -> Point -> Float  -- distancia absoluta
+
+newP x y = Poi x y
+difP (Poi x1 y1) (Poi x2 y2) = sqrt(fromIntegral ((x1 - x2)^2 + ((y1 - y2)^2)))
+
