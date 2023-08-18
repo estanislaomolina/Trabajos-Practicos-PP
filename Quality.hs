a8241fd6c97cbc0fbddb332f909f278f5896dd7f
Estanislao Molina <estanislaomolina05@gmail.com>
2023-08-18 10:14:19 -0300

    link actualizado

--- Quality.hs
+++ Quality.hs
@@ -7,7 +7,5 @@ newQ :: String -> Int -> Float -> Quality
 newQ name tunels delay = Qua name tunels delay
 
 capacityQ :: Quality -> Int -- cuantos túneles puede tolerar esta conexión
-capacityQ (Qua _ tunels _) = tunels
-
 delayQ :: Quality -> Float  -- la demora por unidad de distancia que sucede en las conexiones de este canal
 delayQ (Qua _ _ delay) = delay
