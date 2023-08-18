98a2b6fba62a1d8cfc79d697bfd7f99d19016c18
Estanislao Molina <estanislaomolina05@gmail.com>
2023-08-18 11:02:15 -0300

    mas tests

--- TestCode.hs
+++ TestCode.hs
@@ -8,9 +8,25 @@ import Link
 
 p1 = newP 10 10
 p2 = newP 8 10
+p3 = newP 5 5
 dist = difP p1 p2
 
 c1 = newC "Atlanta" p1
 c2 = newC "Chicago" p2
+c3 = newC "New York" p3
 nombre = nameC c1
 distancia = distanceC c1 c2
+
+q1 = newQ "good" 3 1
+cap = capacityQ q1
+del = delayQ q1
+
+link1 = newL c1 c2 q1
+
+con1 = connectsL c2 link1
+con2 = connectsL c3 link1
+
+linkCon1 :: Bool
+linkCon1 = linksL c1 c2 link1
+linkCon2 = linksL c1 c3 link1
+linkCon3 = linksL c2 c1 link1
