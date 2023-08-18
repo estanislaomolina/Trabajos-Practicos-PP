53545b2cf2354a3d6e7e3df5c455e839072071f5
Estanislao Molina <estanislaomolina05@gmail.com>
2023-08-18 10:33:08 -0300

    Prueba point

--- TestCode.hs
+++ TestCode.hs
@@ -1,5 +1,11 @@
-module TestCode where 
-    
-import Point
+module TestCode 
+    where 
 
-newP x y = Poi x y 
+import Point
+import City
+import Quality
+import Link
+
+p1 = newP 10 10
+p2 = newP 8 10
+dist = difP p1 p2
