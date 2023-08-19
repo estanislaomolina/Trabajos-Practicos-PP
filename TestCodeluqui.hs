d8c83276c1b52d16e678fae1ea149730098475d1
Lucaspini01 <pinil@udesa.edu.ar>
2023-08-19 12:36:19 -0300

    .

---
+++ TestCodeluqui.hs
@@ -0,0 +1,31 @@
+module TestCode 
+    where 
+
+import Point
+import City
+import Quality
+import Link
+import Tunel
+
+p1 = newP 10 10
+p2 = newP 8 10
+p3 = newP 2 1
+p4 = newP 1 1
+
+dist = difP p1 p2
+
+atlanta = newC "Atlanta" p1
+nyc = newC "NYC" p2
+tokyo = newC "Tokyo" p3
+nagasaki = newC "Nagasaki" p4
+
+good = newQ "Good" 3 1.5
+bad = newQ "Bad" 1 0.5
+
+link1 = newL atlanta nyc good
+
+link2 = newL nagasaki tokyo bad
+
+tunel1 = newT [link1,link2]
+
+
--- Tunel.hs
+++ Tunel.hs
@@ -1,17 +1,20 @@
 module Tunel ( Tunel, newT, connectsT, usesT, delayT )
+
    where
 
-import City , Link
+import City 
+import Link
 
 data Tunel = Tun [Link] deriving (Eq, Show)
 
 newT :: [Link] -> Tunel
-newT [link] = Tun [link]
+newT  = Tun
 
 connectsT :: City -> City -> Tunel -> Bool -- indica si este tunel conceta estas dos ciudades distintas
-connectsT 
+connectsT ciudad1 ciudad2 (Tun links) | connectsL ciudad1 (head links) && connectsL ciudad2 (last links) || connectsL ciudad2 (head links) && connectsL ciudad1 (last links) = True | otherwise = False
 
 usesT :: Link -> Tunel -> Bool  -- indica si este tunel atraviesa ese link
-
+usesT linktarget (Tun links) = foldr (\each fold -> each == linktarget || fold ) False links
 
 delayT :: Tunel -> Float -- la demora que sufre una conexion en este tunel
+delayT (Tun links) = foldr (\each fold -> (+) (delayL each) fold) 0 links
