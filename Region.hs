71bce61a1d671bafa003e4a0cfcece3661c2dca6
Lucaspini01 <pinil@udesa.edu.ar>
2023-08-24 22:28:38 -0300

    .

--- Region.hs
+++ Region.hs
@@ -1,4 +1,4 @@
-module Region ( Region, newR, foundR, linkR, tunelR, connectedR, linkedR, delayR, availableCapacityForR)
+module Region ( Region, newR, foundR, linkR, tunelR, connectedR, linkedR) --delayR, availableCapacityForR)
    where
 
 import City
@@ -12,34 +12,30 @@ newR :: Region
 newR = Reg [] [] [] 
 
 foundR :: Region -> City -> Region -- agrega una nueva ciudad a la región
-foundR (Reg citylist _ _) city = reg ((city ++ [citylist]) _ _)
+foundR (Reg citylist linklist tunelist) city = (Reg (citylist ++ [city]) linklist tunelist)
 
 linkR :: Region -> City -> City -> Quality -> Region -- enlaza dos ciudades de la región con un enlace de la calidad indicada
-linkR (Reg citylist linklist tunelist) city1 city2 quality = Reg citylist ((newL city1 city2 quality)++[linklist]) tunelits
+linkR (Reg citylist linklist tunelist) city1 city2 quality = Reg citylist (linklist ++ [(newL city1 city2 quality)]) tunelist
 
 checkcityR :: Region -> City -> Bool -- chequea si una ciudad pertenece a una region
-cityR (Reg citylist _ _) citytarget = foldr (\each fold -> each == citytarget || fold ) False citylist
+checkcityR (Reg citylist _ _) citytarget = foldr (\each fold -> each == citytarget || fold ) False citylist
 
 multicheckcityR :: Region -> [ City ] -> Bool -- cheque si todas las ciudades de la lista pertenecen a la region
-multicheckcityR (Reg regcitylist _ _) citylist = foldr(\each fold -> checkcityR (Reg regcitylist _ _) each || fold ) False citylist
+multicheckcityR (Reg rcitylist rlinklist rtunelist) citylist = foldr(\each fold -> checkcityR (Reg rcitylist rlinklist rtunelist) each || fold ) False citylist
 
-checklinkR :: [ Link ] -> City -> City -> Bool
-checklinkR linklist city1 city2 = foldr (\each fold -> city1 city2 each || fold ) False linklist
-
-multichecklinkR :: [ Link ] -> [ City ] -> Bool
-multichecklinkR linklist citylist = foldr (\(each, next) fold -> checklinkR linklist each next || fold ) False (zip citylist (tail citylist))
+checklinkR :: Region -> [ City ] -> Bool
+checklinkR (Reg rcitylist rlinklist rtunelist) citylist = foldr (\(each, next) fold -> linkedR (Reg rcitylist rlinklist rtunelist) each next || fold ) False (zip citylist (tail citylist))
 
 tunelR :: Region -> [ City ] -> Region -- genera una comunicación entre dos ciudades distintas de la región
-tunelR (Reg regcitylist reglinklist regtunelist) citylist | multicheckcityR (Reg regcitylist _ _) citylist == False = error "Alguna ciudad no pertenece a la region"
-                                                          | 
+tunelR (Reg rcitylist rlinklist rtunelist) citylist | multicheckcityR (Reg rcitylist rlinklist rtunelist) citylist == False = error "Alguna ciudad no pertenece a la region"
 
 connectedR :: Region -> City -> City -> Bool -- indica si estas dos ciudades estan conectadas por un tunel
 connectedR (Reg _ _ tunelist) city1 city2 = foldr (\each fold -> connectsT city1 city2 each  || fold ) False tunelist
 
 
 linkedR :: Region -> City -> City -> Bool -- indica si estas dos ciudades estan enlazadas
-linkedR (Reg _ linklist _) city1 city2 = foldr (\each fold -> connectsL city1 city2 each  || fold ) False linklist
+linkedR (Reg _ linklist _) city1 city2 = foldr (\each fold -> linksL city1 city2 each  || fold ) False linklist
 
-delayR :: Region -> City -> City -> Float -- dadas dos ciudades conectadas, indica la demora
+--delayR :: Region -> City -> City -> Float -- dadas dos ciudades conectadas, indica la demora
 
-availableCapacityForR :: Region -> City -> City -> Int -- indica la capacidad disponible entre dos ciudades
+--availableCapacityForR :: Region -> City -> City -> Int -- indica la capacidad disponible entre dos ciudades
--- Testluqui.hs
+++ Testluqui.hs
@@ -25,7 +25,7 @@ tokyo = newC "Tokyo" p3
 miami = newC "Miami" p4
 bsas = newC "Buenos Aires" p5
 
-Ultra = newQ "Ultra" 10 5
+ultra = newQ "Ultra" 10 5
 prime = newQ "Prime" 5 3
 super = newQ "Super" 4 2 
 good = newQ "Good" 3 1.5
@@ -35,11 +35,11 @@ bad = newQ "Bad" 1 0.5
 link1 = newL atlanta nyc good
 link2 = newL nyc miami mid
 link3 = newL miami tokyo bad
-link4 = newL tokyo bsas Ultra
+link4 = newL tokyo bsas ultra
 
 tunel1 = newT [link1,link2]
 tunel2 = newT [link1,link2,link3,link4]
 tunel3 = newT [link1,link3,link2,link4]
 
-Asia = newR nyc good tunel1
+region1 = (newR [atlanta, nyc, tokyo, miami, bsas] [link1,link2,link3,link4] [tunel1])
 
