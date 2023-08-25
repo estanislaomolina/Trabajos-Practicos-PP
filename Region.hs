118d822bd25bdd437ccd23f4b4f88875785e0f1d
Lucaspini01 <pinil@udesa.edu.ar>
2023-08-25 13:16:59 -0300

    ..

--- Region.hs
+++ Region.hs
@@ -88,72 +88,15 @@ region8 = linkR region7 tokyo miami good
 region9 = linkR region8 miami bsas prime
 
 region10 = linkR region6 tokyo miami mid
-reigon11 = linkR region7 miami bsas super
+region11 = linkR region7 miami bsas super
 
 citylist1 = [bsas, miami, tokyo, nyc, atlanta]
 citylist2 = [bsas,       tokyo, nyc, atlanta]
 citylist3 = [atlanta, nyc]
 citylist4 = [miami, bsas]
 
+checklinkR1 = checklinkR region9 citylist1 -- Devuelve True
+checklinkR2 = checklinkR region11 citylist1 -- Devuelve False
+checklinkR3 = checklinkR region11 citylist4 -- Devuelve True
 
-
-{- p1 = newP 10 10
-p2 = newP 8 10
-p3 = newP 5 5
-dist = difP p1 p2
-
-c1 = newC "Atlanta" p1
-c2 = newC "Chicago" p2
-c3 = newC "New York" p3
-nombre = nameC c1
-distancia = distanceC c1 c2
-
-q1 = newQ "good" 3 1
-q2 = newQ "bad" 5 10
-cap = capacityQ q1
-del = delayQ q1
-
-link1 = newL c1 c2 q1
-link2 = newL c2 c3 q2
-con1 = connectsL c2 link1
-con2 = connectsL c3 link1
-
-linkCon1 :: Bool
-linkCon1 = linksL c1 c2 link1
-linkCon2 = linksL c1 c3 link1
-linkCon3 = linksL c2 c1 link1
-
-capac = capacityL link1
-
-dela = delayL link1
-
-tunel1 = newT [link1]
-tunel2 = newT [link2]
-
-tunCon1 = connectsT c1 c2 tunel1
-tunCon2 = connectsT c1 c3 tunel1
-tunUse1 = usesT link1 tunel1 
-tunUse2 = usesT link1 tunel2
-tunDel1 = delayT tunel1
-
-region1 = newR
-region2 = foundR region1 c1
-region3 = linkR region2 c1 c2 q1
-region4 = foundR region3 c2
-
-checkCit1 = checkcityR region4 c1 -- True
-checkCit2 = checkcityR region4 c2 -- True
-checkCit3 = checkcityR region4 c3 -- False
-
-multcheckCit1 = multicheckcityR region4 [c1, c2]
-multcheckCit2 = multicheckcityR region4 [c1, c2, c3]
-multcheckCit3 = multicheckcityR region4 [c1, c3]
-multcheckCit4 = multicheckcityR region4 [c2]
-
-checkLin1 = checklinkR region4 [c1, c2] -- True
-checkLin2 = checklinkR region4 [c1, c2, c3] -- False
-checkLin3 = checklinkR region4 [c3] -- False
-region5 = linkR region4 c3 c2 q2
-checkLin4 = checklinkR region5 [c1, c2, c3] -- True
- -}
 
