d70f737dce0b49b38ec3bfba45688143d3e60b26
Lucaspini01 <126919016+Lucaspini01@users.noreply.github.com>
2023-09-09 19:07:20 -0300

    Update Queue.java luqui

--- tp2/src/Queue.java
+++ tp2/src/Queue.java
@@ -1,31 +1,38 @@
 import java.util.ArrayList;
 
 public class Queue {
-	ArrayList<Object> lista = new ArrayList<Object>(); //creo una lista vacia de tipo object
-	public boolean isEmpty() {
-		return lista.isEmpty();
+	public ArrayList<Object> queue = new ArrayList<Object>();
+	public static String QueIsEmpty = "Queue is empty";
+	private static final int FirstItemInQueue = 0;
+
+  	public boolean isEmpty() {
+        return queue.isEmpty();
 	}
 
 	public Queue add( Object  cargo ) {
-		lista.add(cargo);
+		queue.add( cargo );
 		return this;
 	}
 
 	public Object take() {
-    // TODO Auto-generated method stub
-		Object elem1 = lista.get(0);
-		lista.remove(0);
-		return elem1;
+		  if (queue.isEmpty()) {
+			  throw new Error(QueIsEmpty);
+		  }
+		  else {return queue.remove( FirstItemInQueue );
+
+		  }
 	}
 
 	public Object head() {
-		// TODO Auto-generated method stub
-    return lista.get(0);
+		  if (queue.isEmpty()) {
+		    throw new Error(QueIsEmpty);
+		  }
+		return queue.get( FirstItemInQueue );
 	}
 
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		  return queue.size();
 	}
 
 }
+
