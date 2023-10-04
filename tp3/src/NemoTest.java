c796e45816f7aaf43875d867519631c5961ded21
Estanislao Molina <estanislaomolina05@gmail.com>
2023-10-04 10:39:04 -0300

    Tests

--- tp3/src/NemoTest.java
+++ tp3/src/NemoTest.java
@@ -18,4 +18,26 @@ public class NemoTest {
         assertEquals(initialDepth, nemo.depth);
         assertEquals(initialCardinalPoint, nemo.cardinalPoint);
     }
+    @ Test public void test01DoNotMoveGivenAnEmptyListOfCommands(){
+        Nemo nemo = new Nemo(initialCoordinateX, initialCoordinateY, initialDepth, initialCardinalPoint);
+
+    }
+    @ Test public void test02MoveOneUnitDown(){
+
+    }
+    @ Test public void test03MoveOneUnitUp(){
+
+    }
+    @ Test public void test04TurnLeft(){
+
+    }
+    @ Test public void test05TurnRight(){
+
+    }
+    @Test public void test06MoveOneUnitForward(){
+
+    }
+    @Test public void test07ReleaseCapsule(){
+
+    }
 }
