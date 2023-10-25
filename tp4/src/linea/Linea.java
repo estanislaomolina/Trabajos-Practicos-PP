2ef31f6c518a86ad30ee51efbfff053a36ac85ff
Estanislao Molina <estanislaomolina05@gmail.com>
2023-10-25 14:08:44 -0300

    TP4 created

---
+++ tp4/src/linea/Linea.java
@@ -0,0 +1,16 @@
+package linea;
+
+public class Linea {
+    public Linea(int prompt, int prompt1, char c) {
+    }
+    public boolean show() {
+        return false;
+    }
+    public boolean finished() {
+        return false;
+    }
+    public void playRedkAt(int prompt) {
+    }
+    public void playBlueAt(int prompt) {
+    }
+}
