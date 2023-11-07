aecb01daa53fa7330e4cb07fef7d6561d3c846c5
Lucaspini01 <pinil@udesa.edu.ar>
2023-11-07 16:06:01 -0300

    Funciona hasta ganar verticalmente. Falta implementar el show()

---
+++ tp4/src/linea/GameMode.java
@@ -0,0 +1,7 @@
+package linea;
+
+public abstract class GameMode {
+
+   public abstract Boolean isGameOver(Linea linea);
+
+}
--- tp4/src/linea/Linea.java
+++ tp4/src/linea/Linea.java
@@ -1,6 +1,7 @@
 package linea;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Linea {
     public static final String NotItsTurnErrorMessage = "No es su turno";
@@ -16,6 +17,7 @@ public class Linea {
     boolean turnoAzul = false;
     int lastPlayedColumn;
     int lastPlayedRow;
+
     public Linea(int prompt, int prompt1, char mode) {
         board = new ArrayList<>();
         gameMode = mode;
@@ -32,12 +34,11 @@ public class Linea {
 
     public boolean show() {
 
-        for (int i = maxHight-1; i >= 0; i--) {
+        for (int i = maxHight - 1; i >= 0; i--) {
             for (int j = 0; j < board.size(); j++) {
                 if (board.get(j).size() > i) {
                     System.out.print("|" + board.get(j).get(i));
-                }
-                else {
+                } else {
                     System.out.print("| ");
                 }
             }
@@ -50,20 +51,46 @@ public class Linea {
         return true;
     }
 
-    public boolean finished() {
-        //return true in gamemode A if there are 4 in a row vertically or horizontally
-        if (gameMode == 'A') {
-            if (board.get(lastPlayedColumn).size() >= 4) {
-                if (board.get(lastPlayedColumn).get(lastPlayedRow).equals(board.get(lastPlayedColumn).get(lastPlayedRow-1)) &&
-                        board.get(lastPlayedColumn).get(lastPlayedRow).equals(board.get(lastPlayedColumn).get(lastPlayedRow-2)) &&
-                        board.get(lastPlayedColumn).get(lastPlayedRow).equals(board.get(lastPlayedColumn).get(lastPlayedRow-3))) {
-                    return true;
-                }
-            }
 
+    public boolean finished() {
+        if (gameMode == 'A') {
+            if (verticalFinish()) {
+                return true;
+            }
         }
         return false;
     }
+
+    //return true in gamemode A if there are 4 in a row vertically or horizontally
+//        if (gameMode == 'A') {
+//            if (board.get(lastPlayedColumn).size() >= 4) {
+//                if (board.get(lastPlayedColumn).get(lastPlayedRow).equals(board.get(lastPlayedColumn).get(lastPlayedRow - 1)) &&
+//                        board.get(lastPlayedColumn).get(lastPlayedRow).equals(board.get(lastPlayedColumn).get(lastPlayedRow - 2)) &&
+//                        board.get(lastPlayedColumn).get(lastPlayedRow).equals(board.get(lastPlayedColumn).get(lastPlayedRow - 3))) {
+//                    return true;
+//                }
+//            }
+//            //chequar usando un for las casillas de la misma fila
+//            //si suman 4 en linea, return true
+//            int count = 0;
+//            if (board.get(lastPlayedColumn).size() >= 4) {
+//                for (int i = 0; i < board.get(lastPlayedColumn).size(); i++) {
+//                    if (board.get(lastPlayedColumn).get(i).equals(board.get(lastPlayedColumn).get(lastPlayedRow))) {
+//                        count++;
+//                    }
+//                    else {
+//                        count = 0;
+//                    }
+//                    if (count >= 4) {
+//                        return true;
+//                    }
+//                }
+//            }
+//        }
+//
+//
+//        return false;
+//    }
     public void playRedAt(int column) {
         if (finished()) {
             throw new RuntimeException(gameFinishedErrorMessage);
@@ -73,13 +100,12 @@ public class Linea {
         }
         if (board.get(column).size() == maxHight) {
             throw new RuntimeException(fullColumnErrorMessage);
-        }
-        else {
+        } else {
             board.get(column).add(RedPlayerSymbol);
             turnoRojo = false;
             turnoAzul = true;
             lastPlayedColumn = column;
-            lastPlayedRow = board.get(column).size()-1;
+            lastPlayedRow = board.get(column).size() - 1;
         }
     }
 
@@ -93,16 +119,30 @@ public class Linea {
         }
         if (board.get(column).size() == maxHight) {
             throw new RuntimeException(fullColumnErrorMessage);
-        }
-        else {
+        } else {
             board.get(column).add(BluePlayerSymbol);
             turnoRojo = true;
             turnoAzul = false;
             lastPlayedColumn = column;
-            lastPlayedRow = board.get(column).size()-1;
+            lastPlayedRow = board.get(column).size() - 1;
         }
     }
 
 
+    public Boolean verticalFinish() {
+        for (int i = 0; i < board.size(); i++) {
+            if (board.get(i).size() >= 4) {
+                if (board.get(i).get(lastPlayedRow).equals(board.get(i).get(lastPlayedRow - 1)) &&
+                        board.get(i).get(lastPlayedRow).equals(board.get(i).get(lastPlayedRow - 2)) &&
+                        board.get(i).get(lastPlayedRow).equals(board.get(i).get(lastPlayedRow - 3))) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
-}
+    public Boolean horizontalFinish() {
+        return false;
+    }
+    }
