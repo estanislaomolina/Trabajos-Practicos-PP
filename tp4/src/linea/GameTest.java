8024d1edddc97abbdc6cf9a9b71b652c0c66ccbe
Estanislao Molina <estanislaomolina05@gmail.com>
2023-11-10 14:04:44 -0300

    tests

--- tp4/src/linea/GameTest.java
+++ tp4/src/linea/GameTest.java
@@ -45,24 +45,66 @@ public class GameTest {
     }
 
     @Test
-    public void test02ChipIsPlacedCorrectlyInBoard() {
+    public void test04ChipIsPlacedCorrectlyInBoardForRedModeA() {
+        linea5x5modeA.playRedAt(1);
+        assertEquals('O', linea5x5modeC.board.get(0).get(0));
+    }
+    @Test
+    public void test05ChipIsPlacedCorrectlyInBoardForRedModeB() {
+        linea5x5modeB.playRedAt(1);
+        assertEquals('O', linea5x5modeC.board.get(0).get(0));
+    }
+    @Test
+    public void test06ChipIsPlacedCorrectlyInBoardForRedModeC() {
         linea5x5modeC.playRedAt(1);
         assertEquals('O', linea5x5modeC.board.get(0).get(0));
     }
 
     @Test
-    public void test03BlueCantPlayFirst() {
+    public void test07BlueCantPlayFirstModeA() {
+        assertThrowsLike(() -> linea5x5modeA.playBlueAt(1), Linea.notItsTurnErrorMessage);
+    }
+    @Test
+    public void test08BlueCantPlayFirstModeB() {
+        assertThrowsLike(() -> linea5x5modeB.playBlueAt(1), Linea.notItsTurnErrorMessage);
+    }
+    @Test
+    public void test09BlueCantPlayFirstModeC() {
         assertThrowsLike(() -> linea5x5modeC.playBlueAt(1), Linea.notItsTurnErrorMessage);
     }
 
     @Test
-    public void test04RedCantPlayIfNotItsTurn() {
+    public void test10RedCantPlayIfNotItsTurnModeA() {
+        linea5x5modeA.playRedAt(1);
+        assertThrowsLike(() -> linea5x5modeA.playRedAt(2), Linea.notItsTurnErrorMessage);
+    }
+    @Test
+    public void test11RedCantPlayIfNotItsTurnModeB() {
+        linea5x5modeB.playRedAt(1);
+        assertThrowsLike(() -> linea5x5modeB.playRedAt(2), Linea.notItsTurnErrorMessage);
+    }
+    @Test
+    public void test12RedCantPlayIfNotItsTurnModeC() {
         linea5x5modeC.playRedAt(1);
         assertThrowsLike(() -> linea5x5modeC.playRedAt(2), Linea.notItsTurnErrorMessage);
     }
 
     @Test
-    public void test04BlueCantPlayIfNotItsTurn() {
+    public void test13BlueCantPlayIfNotItsTurnModeA() {
+        linea5x5modeA.playRedAt(1);
+        linea5x5modeA.playBlueAt(1);
+
+        assertThrowsLike(() -> linea5x5modeA.playBlueAt(2), Linea.notItsTurnErrorMessage);
+    }
+    @Test
+    public void test14BlueCantPlayIfNotItsTurnModeB() {
+        linea5x5modeB.playRedAt(1);
+        linea5x5modeB.playBlueAt(1);
+
+        assertThrowsLike(() -> linea5x5modeB.playBlueAt(2), Linea.notItsTurnErrorMessage);
+    }
+    @Test
+    public void test15BlueCantPlayIfNotItsTurnModeC() {
         linea5x5modeC.playRedAt(1);
         linea5x5modeC.playBlueAt(1);
 
@@ -70,7 +112,27 @@ public class GameTest {
     }
 
     @Test
-    public void test05CantPlayFullColumn() {
+    public void test16CantPlayFullColumnModeA() {
+        linea5x5modeA.playRedAt(1);
+        linea5x5modeA.playBlueAt(1);
+        linea5x5modeA.playRedAt(1);
+        linea5x5modeA.playBlueAt(1);
+        linea5x5modeA.playRedAt(1);
+
+        assertThrowsLike(() -> linea5x5modeA.playBlueAt(1), Linea.fullColumnErrorMessage);
+    }
+    @Test
+    public void test17CantPlayFullColumnModeB() {
+        linea5x5modeB.playRedAt(1);
+        linea5x5modeB.playBlueAt(1);
+        linea5x5modeB.playRedAt(1);
+        linea5x5modeB.playBlueAt(1);
+        linea5x5modeB.playRedAt(1);
+
+        assertThrowsLike(() -> linea5x5modeB.playBlueAt(1), Linea.fullColumnErrorMessage);
+    }
+    @Test
+    public void test18CantPlayFullColumnModeC() {
         linea5x5modeC.playRedAt(1);
         linea5x5modeC.playBlueAt(1);
         linea5x5modeC.playRedAt(1);
@@ -79,15 +141,58 @@ public class GameTest {
 
         assertThrowsLike(() -> linea5x5modeC.playBlueAt(1), Linea.fullColumnErrorMessage);
     }
-
     @Test
-    public void test06ModeAFinishesVertically() {
-        verticalWinnerBoard();
+    public void test19ModeAFinishesVertically() {
+        verticalWinnerBoard(linea5x5modeA);
         assertTrue(linea5x5modeA.finished());
+    }
+    @Test
+    public void test20ModeAFinishesHorizontally() {
+        horizontalWinnerBoard(linea5x5modeA);
+        assertTrue(linea5x5modeA.finished());
+    }
+    @Test
+    public void test21ModeAWontFinishVerticallyBefore4InLine() {
+        linea5x5modeA.playRedAt(1);
+        linea5x5modeA.playBlueAt(2);
+        linea5x5modeA.playRedAt(1);
+        linea5x5modeA.playBlueAt(2);
+        linea5x5modeA.playRedAt(1);
+        linea5x5modeA.playBlueAt(2);
+
+        assertFalse(linea5x5modeA.finished());
     }
 
     @Test
-    public void test07ModeAWontFinishVerticallyBefore4InLine() {
+    public void test22ModeAWontFinishHorizontallyBefore4InLine(){
+        linea5x5modeA.playRedAt(1);
+        linea5x5modeA.playBlueAt(1);
+        linea5x5modeA.playRedAt(2);
+        linea5x5modeA.playBlueAt(2);
+        linea5x5modeA.playRedAt(3);
+        linea5x5modeA.playBlueAt(3);
+
+        assertFalse(linea5x5modeA.finished());
+    }
+    @Test
+    public void test23ModeACantFinishDiagoanlly() {
+        diagonalWinnerBoard(linea5x5modeA);
+        assertFalse(linea5x5modeA.finished());
+    }
+    @Test
+    public void test24CantKeepPlayingAfterGameFinishedModeA() {
+        verticalWinnerBoard(linea5x5modeA);
+        linea5x5modeA.finished();
+
+        assertThrowsLike(() -> linea5x5modeA.playBlueAt(2), Linea.gameFinishedErrorMessage);
+    }
+    @Test
+    public void test25ModeBFinishesDiagonally() {
+        diagonalWinnerBoard(linea5x5modeB);
+        assertTrue(linea5x5modeB.finished());
+    }
+    @Test
+    public void test26ModeBWontFinishBefore4InLine() {
         linea5x5modeB.playRedAt(1);
         linea5x5modeB.playBlueAt(2);
         linea5x5modeB.playRedAt(1);
@@ -97,54 +202,79 @@ public class GameTest {
 
         assertFalse(linea5x5modeB.finished());
     }
-
     @Test
-    public void test08CantKeepPlayingAfterGameFinished() {
-        verticalWinnerBoard();
-        linea5x5modeA.finished();
-
-        assertThrowsLike(() -> linea5x5modeA.playBlueAt(2), Linea.gameFinishedErrorMessage);
+    public void test27ModeBCantFinishVertically() {
+        verticalWinnerBoard(linea5x5modeB);
+        assertFalse(linea5x5modeB.finished());
     }
-
     @Test
-    public void test09ModeAFinishesHorizontally() {
-        horizontalWinnerBoard();
-        assertTrue(linea5x5modeA.finished());
+    public void test28ModeBCantFinishHorizontally() {
+        horizontalWinnerBoard(linea5x5modeB);
+        assertFalse(linea5x5modeB.finished());
     }
-
     @Test
-    public void test10ModeBFinishesDiagonally() {
+    public void test29CantKeepPlayingAfterGameFinishedModeB() {
         diagonalWinnerBoard(linea5x5modeB);
-        assertTrue(linea5x5modeB.finished());
-    }
+        linea5x5modeB.finished();
 
+        assertThrowsLike(() -> linea5x5modeB.playBlueAt(2), Linea.gameFinishedErrorMessage);
+    }
     @Test
-    public void test11ModeCFinishesDiagonally() {
+    public void test30ModeCFinishesHorizontally() {
+        horizontalWinnerBoard(linea5x5modeC);
+        assertTrue(linea5x5modeC.finished());
+    }
+    @Test
+    public void test31ModeCFinishesVertically() {
+        verticalWinnerBoard(linea5x5modeC);
+        assertTrue(linea5x5modeC.finished());
+    }
+    @Test
+    public void test32ModeCFinishesDiaonally() {
         diagonalWinnerBoard(linea5x5modeC);
         assertTrue(linea5x5modeC.finished());
     }
+    @Test
+    public void test33ModeCWontFinishBefore4InLine() {
+        linea5x5modeC.playRedAt(1);
+        linea5x5modeC.playBlueAt(2);
+        linea5x5modeC.playRedAt(1);
+        linea5x5modeC.playBlueAt(2);
+        linea5x5modeC.playRedAt(1);
+        linea5x5modeC.playBlueAt(2);
+
+        assertFalse(linea5x5modeC.finished());
+    }
+    @Test
+    public void test34CantKeepPlayingAfterGameFinishedModeC() {
+        verticalWinnerBoard(linea5x5modeC);
+        linea5x5modeC.finished();
+
+        assertThrowsLike(() -> linea5x5modeC.playBlueAt(2), Linea.gameFinishedErrorMessage);
+    }
+
 
     private void assertThrowsLike(Executable executable, String message) {
         Assertions.assertEquals(message, assertThrows(Exception.class, executable).getMessage());
     }
 
-    private void verticalWinnerBoard() {
-        linea5x5modeA.playRedAt(1);
-        linea5x5modeA.playBlueAt(2);
-        linea5x5modeA.playRedAt(1);
-        linea5x5modeA.playBlueAt(2);
-        linea5x5modeA.playRedAt(1);
-        linea5x5modeA.playBlueAt(2);
-        linea5x5modeA.playRedAt(1);
+    private void verticalWinnerBoard(Linea linea) {
+        linea.playRedAt(1);
+        linea.playBlueAt(2);
+        linea.playRedAt(1);
+        linea.playBlueAt(2);
+        linea.playRedAt(1);
+        linea.playBlueAt(2);
+        linea.playRedAt(1);
     }
-    private void horizontalWinnerBoard() {
-        linea5x5modeA.playRedAt(1);
-        linea5x5modeA.playBlueAt(1);
-        linea5x5modeA.playRedAt(2);
-        linea5x5modeA.playBlueAt(2);
-        linea5x5modeA.playRedAt(3);
-        linea5x5modeA.playBlueAt(3);
-        linea5x5modeA.playRedAt(4);
+    private void horizontalWinnerBoard(Linea linea) {
+        linea.playRedAt(1);
+        linea.playBlueAt(1);
+        linea.playRedAt(2);
+        linea.playBlueAt(2);
+        linea.playRedAt(3);
+        linea.playBlueAt(3);
+        linea.playRedAt(4);
     }
 
 
